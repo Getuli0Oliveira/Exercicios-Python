9e044f1bcaa0c1f9f119ab356492585bacd43d73
Getúlio Oliveira <109239226+Getuli0Oliveira@users.noreply.github.com>
2023-05-22 18:31:44 +0100

    Update main.py
    Aula 5

--- main.py
+++ main.py
@@ -390,6 +390,226 @@ print('Total de pessoas {}'.format(total))
 print('Total de dinheiro {}'.format(dinheiro))
 print('Total de pessoas {}'.format(media))
 
+-------------------------------------------------------
+
+# Funções
+
+# Definição da função
+def realce():
+	print('|', '-'*10,'|')
+
+# Programa principal
+realce()
+print('     Menu')
+realce()
+
+--
+# Definição da função
+def mensagem(msg):
+	print('-' * 30)
+	print(msg)
+	print('-' * 30)
+
+# Programa principal
+mensagem('SISTEMA DE ALUNOS')
+mensagem('...')
+mensagem('...')
+
+--
+# Definição da função
+def soma(a, b):
+	s = a + b 
+	print(s)
+
+# Programa principal
+soma(4,5)
+soma(8,9)
+soma(2,1)
+
+--
+# Definição da função
+def soma(a, b):
+	print('A = {} e B = {}'.format(a, b))
+	s = a + b 
+	print('A soma de A + B = {}'.format(s))
+
+# Programa principal
+soma(4,5)
+soma(8,9)
+soma(2,1)
 	
+--
+# Esta função retorna uma Tupla
+
+# Definição da função
+def contador(*num):
+	tam = len(num)
+	print('Recebi os valores {} e são ao todo {}'.format(num, tam))
+
+# Programa principal	
+contador(2,1,7)
+contador(8,0)
+contador(4,4,7,6,2)
+
+# Paramêtros são dados recebidos pela função 
+
+def sub2(a,b):
+	res = a - b
+	print(res)
+
 	
+sub2(b=7,a=5)
+
+# Parâmetros opcionais: 
+
+def soma3(x = 0,y = 0,z = 0): --> Cabeçalho da função
+	res = x + y + z
+	print(res)
 	
+soma3(1,2,3)
+soma3(1,2) 	#z foi omitido
+soma3(1,) 	#y e z foram omitidos
+soma3()			#x, y e z foram omitidos
+
+# Função adaptável: 
+
+def borda(s1):
+	tam=len(s1)
+	if tam: 
+		print('+','-' * tam,'+')
+		print('|',s1,'|')
+		print('+','-' * tam,'+')
+
+
+borda('Olá, mundo')
+borda('Lógica de programação e Algorítmos')
+
+-------------------------------------------------------------
+# Escopo de variáveis: 
+
+Um escopo é o que determina onde vai ser usada a variável
+
+* Escopo local:
+
+* Criado sempre que uma função é chamada 
+* Variáveis criadas, seja no campo de um parâmetro ou no corpo da função.
+* Essas variáveis só existem dentro daquela própria função.
+* Não pode ser acessada por outras funções. 
+
+* Escopo global:
+	
+* Criado no programa principal.
+* Variáveis globais pertencem a um escopo global e são criadas dentro de um programa principal.
+* Uma variável global existe também em todas as funções invocadas ao longo do programa.
+
+def comida():
+	print(ovos)
+
+ovos = 12  # Variável global
+comida()	 # Variável local
+
+--
+
+def comida():
+	ovos = 12  --> Variável local
+	bacon()
+	print(ovos)
+
+def bacon():
+	ovos = 6 	 --> Variável local
+
+comida()
+	
+# A instrução global: 
+
+def comida():
+	global ovos
+	ovos = 'Comida'
+
+ovos = 'global'
+comida()
+print(ovos)
+
+--------------------------------------------------------------------
+
+# Retorno de valores em funções: 
+
+* Procedimento (procedure) - Uma rotina sem retorno
+* Função - uma rotina que retorna um valor ou dados a quem a invocou
+
+def soma3(x=0,y=0,z=0):
+	res = x + y + z
+	return res
+
+retornando = soma3(1,2,3)
+print(retornando)
+
+# forma alternativa simplificada
+print(soma3(2,2))
+
+--
+
+# Validação de string:
+
+def valida_string(pergunta, min, max):
+	s1 = input(pergunta)
+	tam = len(s1)
+	while ((tam < min) or (tam > max)):
+		s1 = input(pergunta)
+		tam = len(s1)
+	return s1
+
+x = valida_string('Digite uma string: ', 10, 30)
+print('Você digitou a string {}.\n Dado válido. Encerrando o programa...'.format(x))
+
+# Recursos avançados com funções: 
+
+* Exceção
+A sintaxe está correta, mas o erro está na execução do programa. 
+
+while True: 
+	try:
+		x = int(input('Por favor digite um número: '))
+		break
+	except ValueError: 
+		print('Oops! Número inválido. Tente novamente...')
+		
+--
+
+def div():
+	try: 
+		num1 = int(input('Digite um número: '))
+		num2 = int(input('Digite um número: '))
+		res = num1 / num2
+	except ZeroDivisionError:
+		print('Oops! Erro de divisão por zero')
+	except:
+		print('Algo de errado aconteceu...')
+	else:
+		return res
+	finally:
+		print('Executará sempre!')
+
+print(div())
+
+# Função como parâmetro de função.
+
+def imprime_com_condicao(num, fcond):
+	if fcond(num):
+		print(num)
+
+def par(x):
+	return x % 2 == 0
+def impar(x):
+	return not par(x)
+
+
+imprime_com_condicao(5, par)
+
+# Função lambda
+
+res = lambda x: x * x 
+print(res(3))
+
+soma = lambda x, y: x + y
+print(soma(3, 5))
